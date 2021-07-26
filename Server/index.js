4fdea3adbd9dd3e720256b33c9c871a63ebda723
Mantas <gusmantas@gmail.com>
2021-07-26 16:46:44 +0200

    build

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,11 @@ const port = 5000;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, (err) =>{
   if(err) {
